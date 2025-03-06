8a846d2742b3f43eb7e947a645910cdbeb3dcf44
salas <salas@trajano.us.es>
2025-03-06 11:32:44 +0100

    Fichero modificado con cabecera

--- lib/src/main/java/us/dit/Control.java
+++ lib/src/main/java/us/dit/Control.java
@@ -1,1 +1,18 @@
+//Hugo Corona Laserna
+//uvus:TYC7650
+//pass:1741256428
+
+package us.dit;
+
+public class Control {
+
+    public static void main(String[] args) {
+        System.out.println(convertirAMayusculasEInvertir("Hola"));
+    }
+    
+    public static String convertirAMayusculasEInvertir(String texto) {
+        String mayusculas = texto.toUpperCase(); // Convierte la cadena a mayúsculas
+        return new StringBuilder(mayusculas).reverse().toString(); // Invierte la cadena y la devuelve
+    }
+}
 
